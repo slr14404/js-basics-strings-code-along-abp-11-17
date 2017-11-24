ac8d50d724efb5c6084151c7066744d3ab47b7f5
Stephanie D <slr14404@yahoo.com>
2017-11-24 23:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const currentUser = 'Grace Hopper';
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-const shortGreeting = `Welcome, ${currentUser.slice (0,1)}!`;
+const currentUser = 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
