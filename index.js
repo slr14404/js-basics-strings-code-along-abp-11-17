0991cc748553198c2b1fc60a6d9e907e4de68290
Stephanie D <slr14404@yahoo.com>
2017-11-24 23:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const currentUser = 'Grace Hopper'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
